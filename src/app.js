5231fe66c0ae407fd7e7b053cbed82726ba2d335
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-08-30 10:16:01 -0400

    weather app

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
 app.use(express.static(path.join(__dirname, "../public")));
-app.get("", (req, res) => {
-  res.send("Hello Express!");
-});
 
-app.get("/help", (req, res) => {
-  res.send({
-    name: "kekw",
+app.set("view engine", "hbs");
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Andrew CHen",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Andrew Chen",
   });
 });
 
-app.get("/about", (req, res) => {
-  res.send("Title");
+app.get("/help", (req, res) => {
+  res.render("help", {
+    message: "help me baby",
+  });
 });
 app.get("/weather", (req, res) => {
   res.send("kek");
